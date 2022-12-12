3d4d42c4d251f9690b3494fe385758e60fbb0da9
Rifqi Irianto <rifqizzz6@gmail.com>
2022-12-12 20:41:58 +0700

    fix now akhir

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,12 +15,13 @@ class HomeController extends Controller
     public function index()
     {
         $now = Carbon::now();
-        $nowAkhir = $now->subDay()->endOfDay();
+        $nowAkhir = Carbon::now()->subDay()->endOfDay();
 
         $auth = Auth::guard('member')->user();
 
-        $nextAuction = Event::with('auctionProducts.photo')->where('tgl_akhir', '>=', $now)
-            ->where('tgl_mulai', '<=', $nowAkhir)
+        $nextAuction = Event::with('auctionProducts.photo')
+            ->where('tgl_mulai', '<=', $now)
+            ->where('tgl_akhir', '>=', $nowAkhir)
             ->where('status_aktif', 1)
             ->orderBy('tgl_mulai')
             ->get();
