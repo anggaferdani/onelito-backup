de380e47e9fafa6a96aed62b18d8f2dc10d95b04
Rifqi Irianto <rifqizzz6@gmail.com>
2023-02-12 14:48:22 +0700

    update logo lnd hitam email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,9 +2,9 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="{{ url('img/logo-bawah.png') }}" class="logo" alt="Laravel Logo">
+<img src="{{ url('img/onelito_lnd_blck.png') }}" class="logo" alt="Laravel Logo">
 @else
-<img src="{{ url('img/logo-bawah.png') }}" class="logo" alt="Laravel Logo">
+<img src="{{ url('img/onelito_lnd_blck.png') }}" class="logo" alt="Laravel Logo">
 @endif
 </a>
 </td>
