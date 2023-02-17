3be9cff042fa0410e5cdb181fad85c2891bbe533
Rifqi Irianto <rifqizzz6@gmail.com>
2023-02-17 20:52:54 +0700

    use moment

--- resources/views/auction.blade.php
+++ resources/views/auction.blade.php
@@ -272,13 +272,14 @@
 
         function startTimer(addedExtraTime, currentEndTime, val) {
             var currTime = moment(currentTime)
-            var end = moment(currentEndTime);
-            var endTime = new Date(currentEndTime);
+            var endTime = moment(currentEndTime);
+            // var endTime = new Date(currentEndTime);
 
             // Update the count down every 1 second
             var x = setInterval(function() {
                 // Get today's date and time and extend it
-                var now = new Date(currentTime);
+                // var now = new Date(currentTime);
+                var now = moment(currentTime)
 
                 // Find the distance between now and the count down date
                 var duration = endTime - now;
