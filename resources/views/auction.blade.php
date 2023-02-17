7d24d9f0ab6567451424e41fbe9aab97914d8c37
Rifqi Irianto <rifqizzz6@gmail.com>
2023-02-17 12:59:51 +0700

    update total bid position

--- resources/views/auction.blade.php
+++ resources/views/auction.blade.php
@@ -78,7 +78,8 @@
                 <div class="col-12">
                     <img src="{{ $bannerImg }}" class="w-100" alt="...">
                 </div>
-                <div class="bottom col-12">
+            </div>
+            <!-- <div class="bottom col-12"> -->
                     <div class="row justify-content-center">
                         <div class="col-2">
                             <div class="card">
@@ -89,8 +90,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+            <!-- </div> -->
         </div>
         <div class="container res">
             <img src="{{ $bannerImg }}" class="w-100" alt="...">
