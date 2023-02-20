cecd1bdbfccd0b357fe22ceb070b4512d809e80c
Rifqi Irianto <rifqizzz6@gmail.com>
2023-02-20 17:53:29 +0700

    update auction winner

--- app/Http/Controllers/Admin/AuctionWinnerController.php
+++ app/Http/Controllers/Admin/AuctionWinnerController.php
@@ -45,7 +45,7 @@ class AuctionWinnerController extends Controller
             $winners = AuctionWinner::query()
                 ->join('t_log_bidding', 't_pemenang_lelang.id_bidding', '=', 't_log_bidding.id_bidding')
                 ->join('m_ikan_lelang', 't_log_bidding.id_ikan_lelang', '=', 'm_ikan_lelang.id_ikan')
-                ->orderBy('m_ikan_lelang.id_event', 'desc')
+                // ->orderBy('m_ikan_lelang.id_event', 'desc')
                 ->select(
                     'm_ikan_lelang.id_event as id_event',
                 't_log_bidding.id_peserta as id_peserta'
@@ -54,6 +54,7 @@ class AuctionWinnerController extends Controller
                 ->where('t_pemenang_lelang.status_aktif', 1)
                 ->groupBy('id_peserta', 'id_event')
                 ->get()
+                ->sortByDesc('id_event')
                 ;
 
             return DataTables::of($winners)
