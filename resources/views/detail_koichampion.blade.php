c989941383bec5a509c1c696f56d34e32cf8b51f
Rifqi Irianto <rifqizzz6@gmail.com>
2023-06-26 20:25:06 +0700

    update champion koi view

--- resources/views/detail_koichampion.blade.php
+++ resources/views/detail_koichampion.blade.php
@@ -11,8 +11,6 @@
     <div class="container" style="max-width:80%">
         <!-- <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 mb-5"> -->
         <div class="row row-cols-1 row-cols-lg-5 g-2 g-lg-3 mb-5">
-            
-g
             @forelse ($championFishes as $fish)
                 @php
                     $photoChampion = 'img/koi12.jpg';
